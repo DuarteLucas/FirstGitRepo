e97725f2b5677cd6fc8f06eb7d87c189ba91fac0
Duarte Lucas <duarteprista@gmail.com>
2017-10-12 22:32:13 +0000

    Search Results working on minimum voltage

--- LoginWithAuthenticationTest/Controllers/SearchController.cs
+++ LoginWithAuthenticationTest/Controllers/SearchController.cs
@@ -10,33 +10,47 @@ namespace LoginWithAuthenticationTest.Controllers
     public class SearchController : Controller
     {
         // GET: Search
-        public ActionResult ResultProgrammer()
-        {
+        [HttpPost]
+        public ActionResult ResultProgrammer(string SelectedLanguage, string SelectedLocation)
+        { 
             jobEntities1 db = new jobEntities1();
+
+            //Console.WriteLine("---------- TESTE ---------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine(SelectedLanguage);
+            //Console.WriteLine(SelectedLocation);
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine("---------- TESTE ---------");
 
             //IEnumerable <Programador> queryResults =
             //    from prog in db.Programador
             //    where prog.Language.FirstOrDefault(c => c.Name == "Javascript")
             //    select prog;
-        
-        // RADIO_BUTTON IMPLEMENTATION TEMPLATE
-        //    if (RadioButton1.Checked)
-        //    {
-        //        Label1.Text = "You selected " + RadioButton1.Text;
-        //    }
-        //    else if (RadioButton2.Checked)
-        //    {
-        //        Label1.Text = "You selected " + RadioButton2.Text;
-        //    }
-        //    else if (RadioButton3.Checked)
-        //    {
-        //        Label1.Text = "You selected " + RadioButton3.Text;
-        //    }
-        //}
 
-        var result = db.Programador.Where( p => p.Language.Any(l => l.Name == "JavaScript") && p.Location.Contains("Lisboa") ) ;
+            // RADIO_BUTTON IMPLEMENTATION TEMPLATE
+            //    if (RadioButton1.Checked)
+            //    {
+            //        Label1.Text = "You selected " + RadioButton1.Text;
+            //    }
+            //    else if (RadioButton2.Checked)
+            //    {
+            //        Label1.Text = "You selected " + RadioButton2.Text;
+            //    }
+            //    else if (RadioButton3.Checked)
+            //    {
+            //        Label1.Text = "You selected " + RadioButton3.Text;
+            //    }
+            //}
 
-            return View(result);
+            var result = db.Programador.Where( p => p.Language.Any(l => l.Name == SelectedLanguage) && p.Location.Contains(SelectedLocation) ) ;
+
+            return View(result.ToList());
         }
     }
 }
