24b255af8af72d3e3c28c873049c681963d3a89d
Duarte Lucas <duarteprista@gmail.com>
2017-10-15 16:29:11 +0000

    "Cha-Cha-Cha" Changes!!!"

--- LoginWithAuthenticationTest/Models/CompanyOffer.cs
+++ LoginWithAuthenticationTest/Models/CompanyOffer.cs
@@ -11,7 +11,8 @@ namespace LoginWithAuthenticationTest.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class CompanyOffer
     {
         public int CompanyOfferID { get; set; }
